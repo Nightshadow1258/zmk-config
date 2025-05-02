4c9dcdd1e1da70a4024f1ac8f228d05c939d11b2
nightshadow1258 <paul.reisch43@gmail.com>
2025-05-02 13:46:38 +0200

    Testing. Verified locally with zephyr V4.1 -> successful build.

--- boards/arm/stm32f103_test/board.cmake
+++ boards/arm/stm32f103_test/board.cmake
@@ -4,4 +4,6 @@
 
 board_runner_args(jlink "--device=STM32F103C8" "--speed=4000")
 
-include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+
